244c60298ad604803b043b126249a9fafefdebbf
Krunal Patel <Pkrunal616@gmail.com>
2019-07-25 22:32:12 -0400

    Connecting server to database

--- server.js
+++ server.js
@@ -1,88 +1,101 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
-//const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'krunal',
+      database : 'smart-brain'
+    }
+  });
+
+
 const app = express();
 
 app.use(cors())
 
 app.use(bodyPaser.json());
 
-const database = {
-    users: [
-        {
-            id: '101',
-            name: 'jim',
-            email: 'jim@gmail.com',
-            password: 'rem',
-            imageInput: 0,
-            joined: new Date()
-        },
-        {
-            id: '102',
-            name: 'liza',
-            email: 'liza@gmail.com',
-            password: 'deku',
-            imageInput: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password){
-            res.json(database.users[0]);
-        } else{
-            res.status(400).json('Wrong password or email');
-        }
+    db.select('email', 'hash').from('login')
+      .where('email', '=', req.body.email)
+      .then(data => {
+          const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+          if(isValid){
+            return  db.select('*').from('users')
+              .where('email', '=', req.body.email)
+              .then(user => {
+                  res.json(user[0])
+              })
+              .catch(err => res.status(400).json('unable to ge user'))
+          }else{
+            res.status(400).json('Wrong email or password');
+          }
+      })
+      .catch(err => res.status(400).json('Wrong email or password'))
 })
 
 app.post('/register', (req, res) => {
     const {email, name, password } = req.body;
-    database.users.push({
-        id: '103',
-        name: name,
-        email: email,
-        imageInput: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length-1]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        return trx
+        .insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0])
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to join'))
 });
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false; 
 
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-           return res.json(user);
-        }
-    });
-    if(!found){
-        res.status(404).json('u haven\'t been register');
-    }
+    db.select('*').from('users').where({id})
+      .then(user => {
+          if(user.length){
+            res.json(user[0])
+            }else{
+                res.status(400).json('user not found');
+            }
+        })
+    .catch(err => res.status(400).json('user not found'))
 });
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false; 
-
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.imageInput++
-           return res.json(user.imageInput);
-        }
-    });
-    if(!found){
-        res.status(404).json('u haven\'t been register');
-    }
+    db('users').where('id', '=', id)
+    .increment('imageInput', 1)
+    .returning('imageInput')
+    .then(imageInput => {
+        res.json(imageInput[0]);
+    })
+    .catch(err => res.status(400).json('unable to get count'))
 })
 
 app.listen(3001, () => {
